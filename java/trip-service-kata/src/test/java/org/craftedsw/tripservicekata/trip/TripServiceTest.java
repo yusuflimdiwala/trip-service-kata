cf02a213c38e73f26dfc159148739e08092f360b
yusuflimdiwala <yusuf.limdiwala1991@gmail.com>
2024-02-18 14:15:11 +0530

    added test for should throw an exception when user is not logged in and created Seam for UserSession in TripService

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,27 @@
 package org.craftedsw.tripservicekata.trip;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.jupiter.api.Test;
+
 public class TripServiceTest {
-	
+
+    @Test  public void
+    should_throw_an_exception_when_user_is_not_logged_in(){
+
+
+        assertThrows(UserNotLoggedInException.class, () -> {
+            TripService tripService = new TestableTripService();
+            tripService.getTripsByUser(null);
+        } );
+
+    }
+
+    private class TestableTripService extends TripService{
+        @Override
+        protected User getLoggedUser() {
+            return null;
+        }
+    }
 }
